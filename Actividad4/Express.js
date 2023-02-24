bfc398f73c03598e2dd139da7324ea06b3587280
DanielHincapie2004 <dhincapie087@gmail.com>
2023-02-24 08:49:16 -0500

    Crud

--- Actividad4/Express.js
+++ Actividad4/Express.js
@@ -3,16 +3,13 @@ const app = express()
 const host = "localhost"
 const port = 5355
 
-app.use(express.text())
+app.use(express.urlencoded())
+
 
 app.get('/home',(req,res)=>{
     res.send("<h1>Hola desde Home</h1>")
 })
 
-app.get('/home/query',(req,res)=>{
-    console.log(req.query)
-    res.send(`Estos son los querys y la query es ${req.query.saludo}`)
-})
 
 app.get('/home/body',(req,res)=>{
     const contBody = req.body
@@ -21,18 +18,32 @@ app.get('/home/body',(req,res)=>{
     //res.json(contBody)
 })
 
-
-
-
 // app.get('/persona/:name/:lastname',(req,res)=>{
 //     let name = req.params.name
 //     let lastname = req.params.lastname
 //     console.log(req.params)
 //     res.send(`<h1>Hola ${name} ${lastname}</h1>`)
 // })
+
+
+
 app.get('/persona/:name/:lastname',(req,res)=>{
     console.log(req.params)
     res.send(`Hola tu nombre es ${req.params.name} y tu apellido es ${req.params.lastname}`)
+})
+
+app.get('/adios',(req,res)=>{
+    console.log(req.body.nombre)
+})
+
+app.use((req,res,next)=>{
+    if(req.query.nombre === "Daniel"){
+        next()
+    }
+    res.send("No hay query")
+})
+app.get('/home/query',(req,res)=>{
+    res.send(`La query es ${req.query.nombre}`)
 })
 
 app.use((req, res)=>{
